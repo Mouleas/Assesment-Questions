2dfd9a25cf7f5898e32916a7edda73521494ff6d
Mouleas <mouleaswar2001@outlook.com>
2023-03-27 16:58:59 +0530

    initial commit

---
+++ Stocks.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoforCS
+{
+    internal class Test2
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i=0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int left = 0;
+            int right = 1;
+            int profit = 0;
+
+            for (int i=0; i < n-1; i++)
+            {
+                if (arr[left] <= arr[right])
+                {
+                    profit = Math.Max(profit, arr[right] - arr[left]);
+                    right++;
+                }
+                else
+                {
+                    left = right;
+                    right++;
+                }
+            }
+            Console.WriteLine(profit);
+
+        }
+    }
+}
