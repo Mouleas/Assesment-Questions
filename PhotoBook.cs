68d3b995629dfced869098f6aff1503352c0aa72
Mouleas <mouleaswar2001@outlook.com>
2023-03-31 14:41:16 +0530

    initial commit

---
+++ PhotoBook.cs
@@ -0,0 +1,57 @@
+﻿namespace ConsoleApp1
+{
+    public class PhotoBook
+    {
+        private static List<int> photoBooks = new List<int>();
+        public PhotoBook()
+        {
+            photoBooks.Add(16);
+        }
+        public PhotoBook(int num)
+        {
+            photoBooks.Add(num);
+        }
+
+        public static int getNumberOfPages()
+        {
+            return photoBooks.Sum();
+        }
+
+        public static List<int> getPhotoAlbum()
+        {
+            return photoBooks;
+        }
+
+
+    }
+
+    public class AddPhotoBook : PhotoBook
+    {
+        public AddPhotoBook() : base(64) { }
+    }
+
+    public class PhotoBookTest
+    {
+        static void Main(string[] args)
+        {
+            // task 1
+            PhotoBook obj = new PhotoBook();
+            Console.WriteLine(PhotoBook.getNumberOfPages());
+
+            // task 2
+            PhotoBook obj2 = new PhotoBook(32);
+            Console.WriteLine(PhotoBook.getNumberOfPages());
+
+            // task 3
+            AddPhotoBook obj3 = new AddPhotoBook();
+            Console.WriteLine(PhotoBook.getNumberOfPages());
+
+            Console.WriteLine("Photo Album");
+            List<int> list = PhotoBook.getPhotoAlbum();
+            foreach (int i in list)
+            {
+                Console.WriteLine($"Album with pages: {i}");
+            }
+        }
+    }
+}
---
+++ SalaryCredit.cs
@@ -0,0 +1,53 @@
+﻿namespace SalaryCredit
+{
+    public class Program
+    {
+        public string getSalary(double start, double end, double hour, double overtime)
+        {
+            if (start > end)
+            {
+                return "Not a valid input";
+            }
+            else
+            {
+                double tot = 0;
+                if (start >= 9 && end <= 17)
+                {
+                    tot += (end - start) * hour;
+                }
+                else if (start >= 9 && start <= 17 && end > 17) 
+                {
+                    tot += (17 - start) * hour;
+                    tot += (end - 17) * overtime * hour;
+                }
+                else if (start > 17)
+                {
+                    tot += (start - end) * overtime * hour;
+                }
+
+                tot = Math.Round(tot, 2);
+                return "$"+String.Format("{0:0.00}", tot);
+            }
+
+        }
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Entry time:");
+            //double startTime = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Out time: ");
+            //double endTime = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Hourly wage: ");
+            //double hourWage = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Overtime wage: ");
+            //double overtimeWage = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine(new Program().getSalary(startTime, endTime, hourWage, overtimeWage));
+
+            Console.WriteLine(new Program().getSalary(9, 17, 30, 1.5)); // 240
+            Console.WriteLine(new Program().getSalary(16, 18, 30, 1.8)); // 84
+            Console.WriteLine(new Program().getSalary(13.25d, 15, 30, 1.5)); // 52.50
+        }
+    }
+}
