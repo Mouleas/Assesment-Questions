18878f56bc13f285354fc6871f940fc21eeb9b82
Mouleas <mouleaswar2001@outlook.com>
2023-03-31 16:02:52 +0530

    initial commit

---
+++ LicensePlateClass.cs
@@ -0,0 +1,55 @@
+﻿namespace Assesment_2
+{
+    internal class LicensePlateClass
+    {
+        string LicensePlate(string dmvNumber, int n)
+        {
+            string plateNumber = "";
+            string temp = "";
+            foreach (char i in dmvNumber)
+            {
+                if (i != '-')
+                {
+                    temp += i;
+                }
+            }
+
+            int count = 0;
+            for (int i = temp.Length-1; i >= 0; i--)
+            {
+                if (count == n)
+                {
+                    plateNumber += "-";
+                    count = 0;
+                }
+                if (Char.IsLetter(temp[i])){
+                    plateNumber += Char.ToUpper(temp[i]);
+                }
+                else
+                {
+                    plateNumber += temp[i];
+                }
+                count++;
+
+            }
+            char[] reversedString = plateNumber.ToCharArray();
+            Array.Reverse(reversedString);
+            return new string(reversedString);
+
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter dmv number: ");
+            //string dmvNumber = Console.ReadLine();
+            //Console.WriteLine("Enter n: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(new LicensePlateClass().LicensePlate(dmvNumber, n));
+
+            Console.WriteLine(new LicensePlateClass().LicensePlate("5F3Z-2e-9-w", 4));
+            Console.WriteLine(new LicensePlateClass().LicensePlate("2-5g-3-J", 2));
+            Console.WriteLine(new LicensePlateClass().LicensePlate("2-4A0r7-4k", 3));
+            Console.WriteLine(new LicensePlateClass().LicensePlate("nlj-206-fv", 3));
+        }
+    }
+}
---
+++ NewLicense.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assesment_2
+{
+    internal class NewLicense
+    {
+        int License(string myName, int agent, string others)
+        {
+            string[] arr = others.Split(' ');
+
+            List<string> list = new List<string>();
+            foreach (string i in arr)
+            {
+                list.Add(i);
+            }
+            list.Add(myName);
+            list.Sort();
+
+            int totalTime = 0;
+            int count = 0;
+            foreach (string str in list)
+            {
+                if (str.Equals(myName))
+                {
+                    return totalTime+20;
+                }
+                else
+                {
+                    count++;
+                    if (count == agent)
+                    {
+                        count = 0;
+                        totalTime += 20;
+                    }
+                }
+            }
+            return totalTime;
+
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter your name: ");
+            //string myName = Console.ReadLine();
+            //Console.WriteLine("Enter agent count: ");
+            //int agentCount = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter other people name: ");
+            //string others = Console.ReadLine();
+            //Console.WriteLine("Time taken to get new license is: ");
+            //Console.WriteLine(new NewLicense().License(myName, agentCount, others));
+
+
+
+            Console.WriteLine(new NewLicense().License("Eric", 2, "Adam Caroline Rebecca Frank"));
+            Console.WriteLine(new NewLicense().License("Zebediah", 1, "Bob Jim Becky Pat"));
+            Console.WriteLine(new NewLicense().License("Aaron", 3, "Jane Max Olivia Sam"));
+        }
+    }
+}
