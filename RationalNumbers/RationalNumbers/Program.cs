2d8e464baa240b108b09c4429af698f4ed1494f3
Mouleas <mouleaswar2001@outlook.com>
2023-04-12 11:29:12 +0530

    initial commit

---
+++ RationalNumbers/RationalNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RationalNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
